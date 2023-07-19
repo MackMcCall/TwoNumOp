f3ff5302e77d60c11a960e48c866228c14ef5c54
Mack McCall <mackmc9924@gmail.com>
2023-07-19 09:47:14 -0600

    Creating TwoNum Better

---
+++ TwoNumOp/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace TwoNumOp
+{
+    using System;
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("What is the first number you would like to use?");
+            float num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the second number you would like to use?");
+            float num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Which you like to add, subtract, multiply, or divide?");
+            string operation = (Console.ReadLine());
+
+            switch (operation)
+            {
+                case ("add" or "addition" or "+"):
+                    Console.WriteLine(num1 + num2);
+                    break;
+                case ("subtract" or "subtraction" or "-"):
+                    Console.WriteLine(num1 - num2);
+                    break;
+                case ("multiply" or "multiplication" or "*" or "x"):
+                    Console.WriteLine(num1 * num2);
+                    break;
+                case ("divide" or "division" or "/"):
+                    Console.WriteLine(num1 / num2);
+                    break;
+            }
+        }
+    }
+}
+
